ff77b7fa5206bfeb9fa5f882ac3bd5fa8fe02d03
Canavaggio-Thibault <visiteuroffutur29@yahoo.fr>
2023-02-18 23:09:24 +0100

    mouvLaterauxv1

--- Rage/js/Héros.js
+++ Rage/js/Héros.js
@@ -1,6 +1,7 @@
 
 const canvas = document.getElementById('myCanvas');
 const c = canvas.getContext("2d");
+const gravity = 0.098;
 
 c.fillRect(0,0,canvas.width, canvas.height)
 
@@ -8,14 +9,21 @@ class Sprite {
     constructor({position, velocity}) {
         this.position = position
         this.velocity = velocity
+        this.height = 150
     }
     draw() {
         c.fillStyle = 'red'
-       c.fillRect(this.position.x,this.position.y,50,150) 
+       c.fillRect(this.position.x,this.position.y,50,this.height) 
     }
     update() {
         this.draw()
+        this.position.x += this.velocity.x
         this.position.y += this.velocity.y
+
+        if (this.position.y + this.height + this.velocity.y >= canvas.height){
+            this.velocity.y = 0;
+        }else this.velocity.y += gravity
+
     }
 }
 const player = new Sprite({
@@ -25,7 +33,7 @@ const player = new Sprite({
     },
     velocity : {
         x : 0,
-        y : 1,
+        y : 0,
     }
 })
 
@@ -36,19 +44,56 @@ const enemy = new Sprite({
      },
      velocity : {
          x : 0,
-         y : 1,
+         y : 0,
      }
  })
 
 
 console.log(player)
 
+const keys = {
+    q : {
+        pressed: false
+    },
+    d : {
+        pressed : false
+    }
+}
+
 function animate() {
     window.requestAnimationFrame(animate)
-    console.log('go');
     c.clearRect(0,0, canvas.width, canvas.height)
     player.update();
     enemy.update();
+
+    if (keys.q.pressed) {
+        player.velocity.x = -1
+    } else if (keys.d.pressed){
+        player.velocity.x = 1
+    }
 }
 
 animate()
+
+ window.addEventListener('keydown',(event) => {
+    switch (event.key) {
+        case 'd' :
+            keys.d.pressed = true
+            break;
+        case 'q' :
+            keys.q.pressed = true
+            break;
+    }
+    console.log(event.key)
+ })
+
+ window.addEventListener('keyup', (event) =>{
+    switch (event.key) {
+        case 'd' :
+            keys.d.pressed = false
+            break;
+        case 'q' :
+            keys.q.pressed = false
+            break;
+    }
+ })
