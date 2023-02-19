12c747a577db74716664ba998d5f33e5aa0d1374
Canavaggio-Thibault <visiteuroffutur29@yahoo.fr>
2023-02-19 19:08:53 +0100

    Refactoring1

--- Rage/js/script.js
+++
@@ -1,9 +0,0 @@
-import game from game.display.js;
-var Fantômeguerrier = game.display.createSprite("./assets/Fantômeguerrier/Idle.png");
-Fantômeguerrier.setSize(130,150);
-
-window.onload = init;
-function init() {
-    let canvasGame2DContext = myCanvas.getContext('2d');
-    Fantômeguerrier.render(canvasGame2DContext,300,300);
-    }
